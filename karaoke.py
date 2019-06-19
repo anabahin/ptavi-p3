9bb330d3f22382bb43378e36c2bea257e630a63b
anabahin <alinabahin3@gmail.com>
2019-06-19 15:22:25 +0200

    Update karaoke.py
    implementada clase KaraokeLocal y pasado pep8

--- karaoke.py
+++ karaoke.py
@@ -9,34 +9,45 @@ from smallsmilhandler import SmallSMILHandler
 
 usage_error = 'usage error: python3 karaoke.py <file.smil>'
 
-def smil2json(filename, tag_list):
 
-    with open(filename.replace('smil','json'), 'w') as smil_file:
-        json.dump(tag_list, smil_file, indent=3)
+class KaraokeLocal:
 
-def print_tags(tag_list):
+    def __init__(self, filename, att_list):
+        parser = make_parser()
+        SMILHandler = SmallSMILHandler(att_list)
+        parser.setContentHandler(SMILHandler)
+        parser.parse(open(filename))
 
-    tags_str = ''
+        self.tag_list = SMILHandler.get_tags()
 
-    for tag in tag_list:
-        tags_str += tag[0]
-        for att in tag[1]:
-            if tag[1][att] != '':
-                tags_str += '\t' + att + '="' + tag[1][att] + '"'
-        tags_str += '\n'
+    def __str__(self):
+        tags_str = ''
 
-    print(tags_str)
+        for tag in self.tag_list:
+            tags_str += tag[0]
+            for att in tag[1]:
+                if tag[1][att] != '':
+                    tags_str += '\t' + att + '="' + tag[1][att] + '"'
+            tags_str += '\n'
 
-def download_files(filename, tag_list):
+        return tags_str
 
-    for tag in tag_list:
-        for att in tag[1]:
-            if 'http://' in tag[1][att]:
-                name = tag[1][att].split('/')[-1]
-                urlretrieve(tag[1][att], name)
-                tag[1][att] = name
+    def do_local(self):
+        for tag in self.tag_list:
+            for att in tag[1]:
+                if 'http://' in tag[1][att]:
+                    name = tag[1][att].split('/')[-1]
+                    urlretrieve(tag[1][att], name)
+                    tag[1][att] = name
 
-    smil2json(filename, tag_list)
+    def to_json(self, filesmil, filejson=''):
+        if filejson:
+            with open(filejson, 'w') as jsonfile:
+                json.dump(self.tag_list, jsonfile, indent=3)
+        else:
+            with open(filesmil.replace('smil', 'json'), 'w') as jsonfile:
+                json.dump(self.tag_list, jsonfile, indent=3)
+
 
 if __name__ == '__main__':
 
@@ -51,13 +62,10 @@ if __name__ == '__main__':
                 'audio': ['src', 'begin', 'dur'],
                 'textstream': ['src', 'region']}
 
-    parser = make_parser()
-    SMILHandler = SmallSMILHandler(att_list)
-    parser.setContentHandler(SMILHandler)
-    parser.parse(open(filename))
+    karaoke = KaraokeLocal(filename, att_list)
 
-    tags = SMILHandler.get_tags()
-    smil2json(filename, tags)
-    print_tags(tags)
-    download_files(filename, tags)
-    print_tags(tags)
+    print(karaoke)
+    karaoke.to_json(filename)
+    karaoke.do_local()
+    karaoke.to_json(filename, 'local.json')
+    print(karaoke)
